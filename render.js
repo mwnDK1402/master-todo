cbab0420cb2602370c7cd15308e17bd84b54a815
Mathias Wagner Nielsen <mani1019@stud.ek.dk>
2026-08-23 00:39:07 +0200

    Render todo list from todo.yaml

---
+++ render.js
@@ -0,0 +1,58 @@
+const text = await window.yamlReady;
+
+function renderCategory(category) {
+  const section = document.createElement("section");
+  section.className = "category";
+  if (category.mandatory)
+    section.dataset.id = "mandatory";
+
+  const h2 = document.createElement("h2");
+  h2.textContent = category.name;
+  section.append(h2);
+
+  const progress = document.createElement("p");
+  progress.className = "progress";
+  progress.textContent = `? / ${category.items.length} færdige`;
+  section.append(progress);
+
+  const ul = document.createElement("ul");
+  ul.role = "list";
+  section.append(ul);
+
+  for (const item of category.items) {
+    ul.append(renderItem(item));
+  }
+
+  return section;
+}
+
+function renderItem(item) {
+  const li = document.createElement("li");
+
+  const label = document.createElement("label");
+  li.append(label);
+
+  const box = document.createElement("input");
+  box.type = "checkbox";
+  box.dataset.id = item.id;
+  label.append(box);
+
+  label.append(item.title);
+
+  if (item.due) {
+    const iso = item.due.toISOString().slice(0, 10);
+
+    const time = document.createElement("time");
+    time.dateTime = iso;
+    // TODO: Format as Danish date
+    time.textContent = " før " + iso;
+    label.append(time);
+  }
+
+  return li;
+}
+
+const data = jsyaml.load(text);
+for (const category of data.categories) {
+  document.querySelector("body").append(renderCategory(category));
+}
